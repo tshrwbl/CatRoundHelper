fd1a7673ce0b1de6b8df628ef6e39efcb9d5cf8e
Tushar Wabale <wabale97@gmail.com>
2026-07-08 20:41:33 +0530

    sql

--- helper.sql
+++ helper.sql
@@ -1,30 +1,51 @@
--- Query to find 2024 cutoffs below 90 percentile with 2023 and 2022 State & All India (AI) percentiles and ranks
-SELECT
-    ci.College_Code,
-    ci.College_Name,
-    bi.Branch_Code,
-    bi.Branch_Name,
-    bi.Status,
-    bi.Home_University,
-    cc24.Category,
-    -- 2024 State Details
-    cc24.Percentile AS Percentile_2024,
-    cc24.Merit_Rank AS Rank_2024,
-    -- 2023 State Details
-    cc23.Percentile AS Percentile_2023,
-    cc23.Merit_Rank AS Rank_2023,
-    -- 2022 State Details
-    cc22.Percentile AS Percentile_2022,
-    cc22.Merit_Rank AS Rank_2022,
-    -- 2024 All India (AI) Details
-    ai24.Percentile AS AI_Percentile_2024,
-    ai24.Merit_Rank AS AI_Rank_2024,
-    -- 2023 All India (AI) Details
-    ai23.Percentile AS AI_Percentile_2023,
-    ai23.Merit_Rank AS AI_Rank_2023,
-    -- 2022 All India (AI) Details
-    ai22.Percentile AS AI_Percentile_2022,
-    ai22.Merit_Rank AS AI_Rank_2022
+
+SELECT DISTINCT Home_University
+FROM branch_info
+WHERE COALESCE(Home_University, '') NOT IN ('Sant Gadge Baba Amravati University','Punyashlok Ahilyadevi Holkar Solapur University','Gondwana University','Rashtrasant Tukadoji Maharaj Nagpur University','Dr. Babasaheb Ambedkar Marathwada University', 'Shivaji University','Kavayitri Bahinabai Chaudhari North Maharashtra University, Jalgaon','Dr. Babasaheb Ambedkar Technological University,Lonere','Swami Ramanand Teerth Marathwada University, Nanded');
+
+
+Select DISTINCT [Status]
+from branch_info
+where COALESCE([Status], '') NOT LIKE '%Minority%'
+
+SELECT DISTINCT Branch_Name
+FROM branch_info
+WHERE Branch_Name IN (
+    'Robotics and Artificial Intelligence', 
+    'Artificial Intelligence (AI) and Data Science', 
+    'Computer Science and Business Systems', 
+    'Electronics and Computer Science', 
+    'Computer Science and Design', 
+    'Industrial IoT', 
+    'Artificial Intelligence and Data Science', 
+    'Computer Science and Engineering (Artificial Intelligence)', 
+    'Computer Science', 
+    'Computer Science and Engineering (Internet of Things and Cyber Security Including Block Chain', 
+    'Computer Science and Engineering (Cyber Security)', 
+    'Computer Engineering (Software Engineering)', 
+    'Computer Science and Technology', 
+    'Computer Engineering', 
+    'Computer Science and Engineering(Data Science)', 
+    'Artificial Intelligence', 
+    'Computer Engineering (Regional Language)', 
+    'Internet of Things (IoT)', 
+    'Artificial Intelligence and Machine Learning', 
+    'Electrical and Computer Engineering', 
+    'Cyber Security', 
+    'Electronics and Computer Engineering', 
+    'Computer Science and Engineering (Artificial Intelligence and Data Science)', 
+    'Computer Science and Engineering(Artificial Intelligence and Machine Learning)', 
+    'Information Technology', 
+    'Computer Science and Engineering(Cyber Security)', 
+    'Data Science', 
+    'Computer Science and Engineering', 
+    'Computer Technology', 
+    'Computer Science and Information Technology', 
+    'Data Engineering', 
+    'Computer Science and Engineering (IoT)'
+);
+
+Select DISTINCT(ci.College_Name) , ci.College_Code , AVG(cc24.Percentile) as cetAvg, avg(ai24.Percentile) JeeAvg
 FROM cap_cutoffs cc24
     INNER JOIN college_info ci ON cc24.College_Code = ci.College_Code
     INNER JOIN branch_info bi ON cc24.Branch_Code = bi.Branch_Code
@@ -94,6 +115,8 @@ WHERE cc24.Year = 2024
     AND ci.College_Code not in (
         '5162', '5108', '5151', '5139','5331','5121','5181', '5160', '5418', '5109', '5330','5161', --POSSIBLE 
         '6310','6185','6768','6770','6635','6284','6267','1002','4167','6796','6317','5382','5244','6277','6184','6283','6223','6319','6991','1107','6250','6220','6834', '6275','2113','6005', '6628','2008','3215','2020','6004','6815','6214','5004','6268','1105','6222','3139','6187','1114')
+GROUP BY ci.College_Name,  ci.College_Code
+ORDER BY cetAvg DESC
 
-ORDER BY cc24.Percentile DESC;
 
+5162, 5108, 5151, 5139,5331,5121,5181, 5160, 5418, 5109, 5330,5161
